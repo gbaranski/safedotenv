8712ea51bcd15f59090b9f367d28ac08ae82d03d
gbaranski <root@gbaranski.com>
2021-03-10 21:55:18 +0100

    fix: reading lines fn

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,11 @@
-use std::path::Path;
+use std::path::PathBuf;
 use std::fs::File;
 use std::io::{self, BufRead};
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
+pub fn read_lines(path: &PathBuf) -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open(path)?;
     Ok(io::BufReader::new(file).lines())
 }
 
